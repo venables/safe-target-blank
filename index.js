f341c86b129674a0614f99d70c308869eccee48a
Matt Venables <mattvenables@gmail.com>
2017-02-04 14:49:34 -0500

    Add rel=noreferrer after document is loaded

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 /*
- * safe-target-blank v1.0.0
- * Copyright 2014 Matt Venables https://mattvenables.com
+ * safe-target-blank v1.1.0
+ * Copyright 2014-2017 Matt Venables https://mattvenables.com
  */
-(function() {
-  'use strict';
 
-  var documentHasLoaded = setInterval(function () {
+(function () {
+  'use strict'
 
-    if (document.readyState !== "complete") return;
-    clearInterval(documentHasLoaded);
-
-    var links = document.links;
-    for(var i=0; i < links.length; i++) {
-      if (links[i].target == '_blank' && links[i].rel.indexOf('noreferrer') == -1) {
-        links[i].rel += ' noreferrer';
+  document.addEventListener('DOMContentLoaded', function (e) {
+    var links = document.links
+    for (var i = 0; i < document.links.length; i++) {
+      if (links[i].target === '_blank' && links[i].rel.indexOf('noreferrer') === -1) {
+        links[i].rel = (links[i].rel + ' noreferrer').trim()
       }
     }
-
-  }, 30);
-})();
+  })
+})()
